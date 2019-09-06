817e0b2ef33d6bb9cf8ac6b3cb84ec5d48f40bbb
Daniil Kozlov <d.kzlv.ru@gmail.com>
2019-09-06 20:38:57 +0300

    Making awesome image masks for logo

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,6 +1,6 @@
 <script>
-  const classes = ['circle', 'arrow', 'cross', 'border', 'star', 'heart']
-  let lastIndex = 2
+  const classes = ['cross', 'skull', 'fire', 'peace', 'cannabis', 'heart']
+  let lastIndex = 0
 
   const setNewClass = () =>
     lastIndex + 1 === classes.length ? (lastIndex = 0) : lastIndex++
@@ -25,75 +25,32 @@
 <style type="text/scss">
   .logo {
     width: 80px;
-    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
     user-select: none;
-  }
-
-  .circle {
-    clip-path: circle(50% at 50% 50%);
-  }
-
-  .arrow {
-    clip-path: polygon(
-      0% 20%,
-      60% 20%,
-      60% 0%,
-      100% 50%,
-      60% 100%,
-      60% 80%,
-      0% 80%
-    );
+    mask-image: url(/img/skull.svg);
   }
 
   .cross {
-    clip-path: polygon(
-      20% 0%,
-      0% 20%,
-      30% 50%,
-      0% 80%,
-      20% 100%,
-      50% 70%,
-      80% 100%,
-      100% 80%,
-      70% 50%,
-      100% 20%,
-      80% 0%,
-      50% 30%
-    );
+    mask-image: url(/img/cross.svg);
   }
 
-  .border {
-    clip-path: polygon(
-      0% 0%,
-      0% 100%,
-      25% 100%,
-      25% 25%,
-      75% 25%,
-      75% 75%,
-      25% 75%,
-      25% 100%,
-      100% 100%,
-      100% 0%
-    );
+  .skull {
+    mask-image: url(/img/skull.svg);
   }
 
-  .star {
-    clip-path: polygon(
-      50% 0%,
-      61% 35%,
-      98% 35%,
-      68% 57%,
-      79% 91%,
-      50% 70%,
-      21% 91%,
-      32% 57%,
-      2% 35%,
-      39% 35%
-    );
+  .fire {
+    mask-image: url(/img/fire.svg);
+  }
+
+  .peace {
+    mask-image: url(/img/peace.svg);
+  }
+
+  .cannabis {
+    mask-image: url(/img/cannabis.svg);
   }
 
   .heart {
-    clip-path: polygon(50% 90%, 100% 30%, 75% 0, 50% 30%, 25% 0, 0 30%);
+    mask-image: url(/img/heart.svg);
   }
 </style>
 
