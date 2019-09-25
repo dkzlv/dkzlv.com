673248f2b61a090e950d9ebfbdb29058d15163ca
Daniil Kozlov <d.kzlv.ru@gmail.com>
2019-09-25 19:10:31 +0300

    Support for a beautiful custom email collector right inside the post itself

--- src/components/post.svelte
+++ src/components/post.svelte
@@ -1,39 +1,26 @@
 <script>
   import Meta from './meta.svelte'
-  import EmailCollector from './emailCollector.svelte'
-  import t from '../routes/i18n.js'
-  import { langStore } from '../routes/store.js'
-
-  $: endPostEngagement = $langStore && t('email.endPostEngagement')
+  import Subscription from './subscription.svelte'
+  import { onMount, tick } from 'svelte'
 
   export let post
   export let lang
+
+  onMount(async () => {
+    // Тут вот бажина в Svelte: https://github.com/sveltejs/svelte/issues/3218
+    // await можно будет убрать после фикса. Сейчас это выглядит плохо (моргает после первой загрузки), но
+    // хотя бы работает!
+    await tick()
+    const el = document.querySelector('.email-collector')
+    if (!el) return
+
+    const engagement = el.innerText || undefined
+    el.innerText = ''
+    el.classList.remove('email-collector')
+    const cmp = new Subscription({ target: el, props: { engagement } })
+    return () => cmp.$destroy()
+  })
 </script>
-
-<style type="text/scss">
-  @import '../styles/importable';
-
-  .subscription {
-    background-color: $email-collector-background;
-    border-radius: 5px;
-
-    @include mq($until: mobile) {
-      width: 110%;
-      margin: 40px -20px 40px -20px;
-      padding: 20px 40px;
-    }
-    @include mq($from: mobile) {
-      width: 500px;
-      margin: 40px auto 40px auto;
-      padding: 20px 30px;
-
-      box-shadow: 0px 0px 15px 0px $email-collector-shadow-color;
-      &:hover {
-        box-shadow: 0px 0px 25px 0px $email-collector-shadow-color;
-      }
-    }
-  }
-</style>
 
 <Meta
   title={post.meta.title}
@@ -51,10 +38,4 @@
   </div>
 </article>
 
-<div class="subscription">
-  <p>
-    {@html post.meta.emailCollectorMessage || endPostEngagement}
-  </p>
-
-  <EmailCollector />
-</div>
+<Subscription engagement={post.meta.emailCollectorMessage} />
---
+++ src/components/subscription.svelte
@@ -0,0 +1,41 @@
+<script>
+  import EmailCollector from './emailCollector.svelte'
+  import t from '../routes/i18n.js'
+  import { langStore } from '../routes/store.js'
+
+  $: endPostEngagement = $langStore && t('email.endPostEngagement')
+  export let engagement
+</script>
+
+<style type="text/scss">
+  @import '../styles/importable';
+
+  .subscription {
+    background-color: $email-collector-background;
+    border-radius: 5px;
+
+    @include mq($until: mobile) {
+      width: 110%;
+      margin: 40px -20px 40px -20px;
+      padding: 20px 40px;
+    }
+    @include mq($from: mobile) {
+      width: 500px;
+      margin: 40px auto 40px auto;
+      padding: 20px 30px;
+
+      box-shadow: 0px 0px 15px 0px $email-collector-shadow-color;
+      &:hover {
+        box-shadow: 0px 0px 25px 0px $email-collector-shadow-color;
+      }
+    }
+  }
+</style>
+
+<div class="subscription">
+  <p>
+    {@html engagement || endPostEngagement}
+  </p>
+
+  <EmailCollector />
+</div>
