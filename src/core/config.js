9f9cdaafa7260ca25c1b3a5458de7f0b08f2b17e
Daniil Kozlov <d.kzlv.ru@gmail.com>
2020-03-26 12:36:59 +0300

    Changing path construction strategy to dotenv-based

--- src/core/config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-export const scheme = isProd ? 'https' : 'http'
-export const host = isProd ? 'dkzlv.com' : 'blog'
-export const clientPort = isProd ? '' : ':3000'
-export const backPort = isProd ? '' : ':8080'
-
-export const baseBackPath = `${scheme}://api.${host}${backPort}`
-export const baseClientPath = `${scheme}://${host}${clientPort}`
