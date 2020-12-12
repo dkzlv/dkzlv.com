a14d479ea8c50fe4e3ffeaed975d638f05bec46d
Daniil Kozlov <d.kzlv.ru@gmail.com>
2020-12-12 11:57:13 +0300

    Adjusting read time

--- src/core/content/post/calcReadTime.ts
+++ src/core/content/post/calcReadTime.ts
@@ -1,2 +1,2 @@
-// 900 chars per minute on average.
-export const calcReadTime = (charCount: number) => Math.round(charCount / 900) || 1;
+// 900 chars per minute on average. Increasing it to 1100, because we have many special stuff in the post
+export const calcReadTime = (charCount: number) => Math.round(charCount / 1100) || 1;
