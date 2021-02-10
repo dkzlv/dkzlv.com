748cc4c9ddcd065af4e71565df13082747997b48
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-02-10 17:59:59 +0300

    Making simple redirect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   import { goto } from '@sapper/app';
   import { locales } from 'svelte-i18n';
 
-  if (process.env.BROWSER) goto('./' + navigator.language.slice(0, 2));
+  if (process.env.BROWSER) goto('./' + navigator.language.slice(0, 2), { replaceState: true });
 </script>
 
 <div style="display: none">
