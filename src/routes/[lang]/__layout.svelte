23fb994f3476f693de5152f2b590f495ec8fd719
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-08-03 15:20:44 +0300

    Fixing lang redirection for doubles (en_GB) or unknown languages (de_DE)

--- src/routes/[lang]/__layout.svelte
+++ src/routes/[lang]/__layout.svelte
@@ -1,21 +1,29 @@
+<script context="module">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = ({ page }) => {
+    const cleanedLang = page.params.lang?.startsWith('ru') ? 'ru' : 'en';
+
+    if (page.params.lang !== cleanedLang) return { status: 301, redirect: `/${cleanedLang}` };
+
+    return {};
+  };
+</script>
+
 <script>
   import Header from '$components/header.svelte';
   import Footer from '$components/footer/index.svelte';
   import ColorThemes from '$components/colorThemes.svelte';
 
   import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
-  import { locale, locales, _ } from 'svelte-i18n';
+  import { _, locale } from 'svelte-i18n';
 
   import { warn } from '$core/consoleWarning';
 
-  $: {
-    locale.set($page.params.lang);
-    if (!$locales.includes($page.params.lang as string)) goto('/en', { replaceState: true });
-  }
-
   onMount(() => warn($_('consoleWarning')));
+
+  $: $locale = $page.params.lang;
 </script>
 
 <ColorThemes />
